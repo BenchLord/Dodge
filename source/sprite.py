887ca4248ee444bf3cb312d139dadb719dee59b2
BrandonBench <thechummiest@gmail.com>
2014-05-07 11:37:25 -0600

    fix whatever the heck happened

--- source/sprite.py
+++ source/sprite.py
@@ -3,7 +3,6 @@ import game
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, texture, position, align):
-<<<<<<< HEAD
         self.texture = texture
         self.image 	 = pygame.image.load(game.rpath + texture)
         self.rect 	 = self.image.get_rect()
@@ -11,14 +10,6 @@ class Sprite(pygame.sprite.Sprite):
      	self.rect.y  = position[1]
         self.alive   = False
         self.align   = align
-=======
-        self.image 	= pygame.image.load(game.rpath + texture)
-        self.rect 	= self.image.get_rect()
-     	self.rect.x = position[0]
-     	self.rect.y = position[1]
-        self.alive  = False
-        self.align  = align
->>>>>>> e912b0e9c048cf12b05069addab8840d86a91706
         return
 
     def draw(self):
